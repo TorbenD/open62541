68275aaa628527a36848881358e39a90d82c304b
TorbenD <t.deppe@plt.rwth-aachen.de>
2016-06-23 18:56:51 +0200

    Servent Example

--- examples/client_firstSteps.c
+++ examples/client_firstSteps.c
@@ -10,86 +10,41 @@
 #else
 # include "open62541.h"
 #endif
-#include "networklayer_tcp.h"
-#include "..//src//client//ua_client_internal.h"
-#include "..//src//server//ua_services.h"
-#include "ua_server.h"
-#include "..//src//server//ua_server_internal.h"
-
-#ifdef UA_ENABLE_SERVENT
-	#include "ua_servent.h"
-#endif
-
-
-UA_Boolean running = true;
-int a0 = 1;
-int a1 = 0;
-int count = 0;
-static void stopHandler(int sig) {
-    running = false;
-}
 
 int main(void) {
+    UA_Client *client = UA_Client_new(UA_ClientConfig_standard);
+    UA_StatusCode retval = UA_Client_connect(client, "opc.tcp://localhost:16664");
+    if(retval != UA_STATUSCODE_GOOD) {
+        UA_Client_delete(client);
+        return (int)retval;
+    }
 
-#ifdef UA_ENABLE_SERVENT
+    //variables to store data
+    UA_DateTime raw_date = 0;
+    UA_String string_date;
 
-    UA_ServerConfig config = UA_ServerConfig_standard;
-	UA_ServerNetworkLayer nl = UA_ServerNetworkLayerTCP(UA_ConnectionConfig_standard, 16665);
-	config.networkLayers = &nl;
-	config.networkLayersSize = 1;
+    UA_ReadRequest rReq;
+    UA_ReadRequest_init(&rReq);
+    rReq.nodesToRead = UA_Array_new(1, &UA_TYPES[UA_TYPES_READVALUEID]);
+    rReq.nodesToReadSize = 1;
+    rReq.nodesToRead[0].nodeId = UA_NODEID_NUMERIC(0, 2258);
+    rReq.nodesToRead[0].attributeId = UA_ATTRIBUTEID_VALUE;
 
-	UA_Servent *servent = UA_Servent_new(config);
-	UA_Client *client = NULL;
+    UA_ReadResponse rResp = UA_Client_Service_read(client, rReq);
+    if(rResp.responseHeader.serviceResult == UA_STATUSCODE_GOOD && rResp.resultsSize > 0 &&
+       rResp.results[0].hasValue && UA_Variant_isScalar(&rResp.results[0].value) &&
+       rResp.results[0].value.type == &UA_TYPES[UA_TYPES_DATETIME]) {
+        raw_date = *(UA_DateTime*)rResp.results[0].value.data;
+        printf("raw date is: %" PRId64 "\n", raw_date);
+        string_date = UA_DateTime_toString(raw_date);
+        printf("string date is: %.*s\n", (int)string_date.length, string_date.data);
+    }
 
-	UA_StatusCode retval = UA_Server_run_startup(servent->server);
+    UA_ReadRequest_deleteMembers(&rReq);
+    UA_ReadResponse_deleteMembers(&rResp);
+    UA_String_deleteMembers(&string_date);
 
-    while (running)
-		{
-    	retval = UA_Server_run_iterate(servent->server, true);
-    	if (a0 == 1)
-    		{
-    		client = UA_Servent_connect(servent, UA_ClientConfig_standard, "opc.tcp://127.0.0.1:16664", nl);
-			if(!client)
-				{
-				printf("\nClient could not connect");
-				return (int)retval;
-				}
-			a0 = 0;
-    		}
-    	if (a1 == 1)
-    		{
-    		count++;
-    		if (count % 100000000)
-    			{
-			//variables to store data
-			UA_DateTime raw_date = 0;
-			UA_String string_date;
-
-			UA_ReadRequest rReq;
-			UA_ReadRequest_init(&rReq);
-			rReq.nodesToRead = UA_Array_new(1, &UA_TYPES[UA_TYPES_READVALUEID]);
-			rReq.nodesToReadSize = 1;
-			rReq.nodesToRead[0].nodeId = UA_NODEID_NUMERIC(0, 2258);
-			rReq.nodesToRead[0].attributeId = UA_ATTRIBUTEID_VALUE;
-
-			UA_ReadResponse rResp = UA_Client_Service_read(client, rReq);
-			if(rResp.responseHeader.serviceResult == UA_STATUSCODE_GOOD && rResp.resultsSize > 0 &&
-			   rResp.results[0].hasValue && UA_Variant_isScalar(&rResp.results[0].value) &&
-			   rResp.results[0].value.type == &UA_TYPES[UA_TYPES_DATETIME])
-				{
-				raw_date = *(UA_DateTime*)rResp.results[0].value.data;
-				printf("raw date is: %" PRId64 "\n", raw_date);
-				string_date = UA_DateTime_toString(raw_date);
-				printf("string date is: %.*s\n", (int)string_date.length, string_date.data);
-				}
-			UA_ReadRequest_deleteMembers(&rReq);
-			UA_ReadResponse_deleteMembers(&rResp);
-			UA_String_deleteMembers(&string_date);
-    		}
-    		}
-		};
-	UA_Servent_delete(servent);
-	nl.deleteMembers(&nl);
-#endif
+    UA_Client_disconnect(client);
+    UA_Client_delete(client);
     return (int) UA_STATUSCODE_GOOD;
 }
